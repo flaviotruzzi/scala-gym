46fb4d9367ef994afdb4b276f73ef83d19f17f5e
Flavio Sales Truzzi <flaviotruzzi@gmail.com>
2016-06-20 22:02:34 +0200

    Update to new gym-server api

--- src/main/scala/gym/GymClient.scala
+++ src/main/scala/gym/GymClient.scala
@@ -1,8 +1,14 @@
 package gym
 
+import java.io.IOException
+
 import akka.actor.{Actor, ActorLogging}
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.client.RequestBuilding
+import akka.http.scaladsl.model.StatusCodes._
+import akka.http.scaladsl.model._
+import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings}
 import gym.Action
 
@@ -10,34 +16,128 @@ import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-
-trait GymClient extends Actor with ActorLogging with JsonSupport {
+trait GymClient extends Actor with ActorLogging with JsonSupport with GymEndPointsV1 {
 
   import context.dispatcher
   import spray.json._
 
-  val gymServer: GymServer
+  lazy val connectionFlow: Flow[HttpRequest, HttpResponse, Any] =
+    http.outgoingConnection(gymServer.docker.getHost, gymServer.port.toInt)
+  val gymServer: DockerGymServer
+  val environment: String
+  val render: Boolean
   val http = Http(context.system)
   val timeout = 5 seconds
 
   implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(context.system))
+  var instanceId: String = ""
 
   override def preStart() = {
-    self ! Initialize
+    create(environment).map { env =>
+      instanceId = env.instance_id
+      self ! Initialize
+    }
   }
 
-  protected def sendAction(action: Action): Future[StepResponse] = http
-    .singleRequest(HttpRequest(uri = gymServer.actEndpoint(action)))
-    .flatMap(r => r.entity.toStrict(timeout))
-    .map(_.data.decodeString("UTF-8").parseJson.convertTo[StepResponse])
+  def create(env: String): Future[EnvResponse] = {
+    mkRequest(RequestBuilding.Post(v1.create, mkEntity(EnvRequest(env).toJson))).flatMap { response =>
+      response.status match {
+        case Created => Unmarshal(response.entity).to[EnvResponse]
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          Future.failed(new IOException(s"FAIL - ${response.status}"))
+        }
+      }
+    }
+  }
 
-  protected def initialize(): Future[StepResponse] = http
-    .singleRequest(HttpRequest(uri = gymServer.resetEndpoint()))
-    .flatMap(r => r.entity.toStrict(timeout))
-    .map(_.data.decodeString("UTF-8").parseJson.convertTo[InitResponse])
-    .map(initResponse => initResponse.toStepResponse)
+  def step(action: Action): Future[ActionResponse] = {
+    mkRequest(RequestBuilding.Post(v1.step(instanceId), mkEntity(ActionRequest(action, render).toJson))).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[ActionResponse]
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          Future.failed(new IOException(s"FAIL - ${response.status}"))
+        }
+      }
+    }
+  }
+
+  def mkEntity(json: JsValue) = HttpEntity(ContentTypes.`application/json`, json.toString())
+
+  def monitorStart(force: Boolean, resume: Boolean): Future[Boolean] = {
+    mkRequest(RequestBuilding.Post(v1.monitorStart(instanceId), mkEntity(MonitorStartRequest(force, resume).toJson))).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[GenericResponse].map(_.message)
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          Future.failed(new IOException(s"FAIL - ${response.status}"))
+        }
+      }
+    }
+  }
+
+  def monitorStop(): Future[Boolean] = {
+    mkRequest(RequestBuilding.Post(v1.monitorStop(instanceId))).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[GenericResponse].map(_.message)
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          Future.failed(new IOException(s"FAIL - ${response.status}"))
+        }
+      }
+    }
+  }
+
+  def mkRequest(request: HttpRequest): Future[HttpResponse] =
+    Source.single(request).via(connectionFlow).runWith(Sink.head)
+
+  def upload(algorithmId: String, writeup: String, apiKey: String, ignoreOpenMonitors: Boolean) = {
+    mkRequest(RequestBuilding.Post(v1.upload(instanceId), mkEntity(UploadRequest(algorithmId, writeup, apiKey, ignoreOpenMonitors).toJson))).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[GenericResponse].map(_.message)
+        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
+          Future.failed(new IOException(s"FAIL - ${response.status}"))
+        }
+      }
+    }
+  }
+
+  def info(instanceId: String) = {
+    throw new NotImplementedError()
+  }
+
+  protected def initialize(): Future[ActionResponse] = reset()
+
+  def reset(): Future[ActionResponse] = {
+    mkRequest(RequestBuilding.Post(v1.reset(instanceId), mkEntity(ResetRequest(render).toJson))).flatMap { response =>
+      response.status match {
+        case OK => Unmarshal(response.entity).to[InitResponse].map(_.toActionResponse)
+        case _ =>
+          Unmarshal(response.entity).to[String].flatMap { entity =>
+            Future.failed(new IOException(s"FAIL - ${response.status}"))
+          }
+      }
+    }
+  }
 }
 
 
+trait GymEndPointsV1 {
 
+  object v1 {
+    def help = "/v1/help"
+
+    def create = "/v1/envs/create/"
+
+    def info(instanceId: String) = s"/v1/envs/$instanceId/info"
+
+    def reset(instanceId: String) = s"/v1/envs/$instanceId/reset/"
+
+    def step(instanceId: String) = s"/v1/envs/$instanceId/step/"
+
+    def monitorStart(instanceId: String) = s"/v1/envs/$instanceId/monitor/start/"
+
+    def monitorStop(instanceId: String) = s"/v1/envs/$instanceId/monitor/stop/"
+
+    def upload(instanceId: String) = s"/v1/envs/$instanceId/upload/"
+  }
+
+}
 
--- src/main/scala/gym/GymMessages.scala
+++ src/main/scala/gym/GymMessages.scala
@@ -8,11 +8,32 @@ package object gym {
   type State = Int
 }
 
-case class StepResponse(done: Boolean, info: Map[String, String], observation: List[Double], reward: Double)
 
-case class InitResponse(observation: List[Double]) {
-  def toStepResponse = StepResponse(done = false, Map.empty, observation, 0)
+import gym._
+
+trait APIEntity
+
+case class EnvRequest(environment: String) extends APIEntity
+case class ResetRequest(render: Boolean) extends APIEntity
+case class ActionRequest(action: Action, render: Boolean) extends APIEntity
+case class MonitorStartRequest(force: Boolean, resume: Boolean) extends APIEntity
+case class UploadRequest(algorithm_id: String, writeup: String, api_key: String, ignore_open_monitors: Boolean) extends APIEntity
+
+case class GenericResponse(message: Boolean) extends APIEntity
+case class EnvResponse(instance_id: String) extends APIEntity
+
+
+case class ActionResponse(done: Boolean,
+                          info: Map[String, String],
+                          observation: List[Double],
+                          reward: Double,
+                          render: Option[String]) extends APIEntity
+
+
+case class InitResponse(observation: List[Double], render: Option[String]) {
+  def toActionResponse = ActionResponse(done = false, Map.empty, observation, 0, render)
 }
+
 
 case object Initialize
 
@@ -20,11 +41,18 @@ case object EndOfEpisode
 
 case class Act[T](state: gym.State)
 
-case class Update[A](observation: StepResponse, lastState: Option[gym.State], lastAction: Option[gym.Action])
+case class Update[A](observation: ActionResponse, lastState: Option[gym.State], lastAction: Option[gym.Action])
 
 trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol {
-  implicit val stepResponseFormat = jsonFormat4(StepResponse)
-  implicit val initResponseFormat = jsonFormat1(InitResponse)
+  implicit val stepResponseFormat = jsonFormat5(ActionResponse)
+  implicit val initResponseFormat = jsonFormat2(InitResponse)
+  implicit val envRequestFormat = jsonFormat1(EnvRequest)
+  implicit val envResponseFormat = jsonFormat1(EnvResponse)
+  implicit val envResetRequestFormat = jsonFormat1(ResetRequest)
+  implicit val actionRequestFormat = jsonFormat2(ActionRequest)
+  implicit val monitorStartRequestFormat = jsonFormat2(MonitorStartRequest)
+  implicit val genericResponseFormat = jsonFormat1(GenericResponse)
+  implicit val uploadRequestFormat = jsonFormat4(UploadRequest)
 }
 
 
